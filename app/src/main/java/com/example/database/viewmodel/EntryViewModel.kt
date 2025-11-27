b0c29803f338db2fb01e44adea74478756f228dd
Pradipa Y <pdpyyo@gmail.com>
2025-11-27 16:48:43 +0700

    membuat data class DetailMahasiswa pada EntryViewModel.kt

--- app/src/main/java/com/example/database/viewmodel/EntryViewModel.kt
+++ app/src/main/java/com/example/database/viewmodel/EntryViewModel.kt
@@ -1,2 +1,14 @@
 package com.example.database.viewmodel
 
+data class DetailSiswa(
+    val id: Int = 0,
+    val nama: String = "",
+    val alamat: String = "",
+    val telpon: String = "",
+)
+
+
+
+fun Siswa.toDetailSiswa(): DetailSiswa = DetailSiswa(
+    id = id,
+)
