fc4d140ca5286563d2577967cc25a19912105688
Pradipa Y <pdpyyo@gmail.com>
2025-11-27 16:52:14 +0700

    membuat funtion siswaTouiStatesiswa pada EntryViewModel.kt

--- app/src/main/java/com/example/database/viewmodel/EntryViewModel.kt
+++ app/src/main/java/com/example/database/viewmodel/EntryViewModel.kt
@@ -16,6 +16,10 @@ fun DetailSiswa.toSiswa(): Siswa = Siswa(
     telpon = telpon
 )
 
+fun Siswa.toUiStateSiswa(isEntryValid:Boolean = false): UIStateSiswa = UIStateSiswa(
+    detailSiswa = this.toDetailSiswa(),
+    isEntryValid = isEntryValid
+)
 
 
 fun Siswa.toDetailSiswa(): DetailSiswa = DetailSiswa(
