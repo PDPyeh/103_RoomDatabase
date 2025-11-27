d1e9b998ef0ce7be1feedf8d140f35fd1774c521
Pradipa Y <pdpyyo@gmail.com>
2025-11-27 18:26:17 +0700

    membuat funtion save siswa pada EntryViewModel.kt

--- app/src/main/java/com/example/database/viewmodel/EntryViewModel.kt
+++ app/src/main/java/com/example/database/viewmodel/EntryViewModel.kt
@@ -1,5 +1,6 @@
 package com.example.database.viewmodel
 
+
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
@@ -7,21 +8,45 @@ import androidx.lifecycle.ViewModel
 import com.example.database.repositori.RepositoriSiswa
 import com.example.database.room.Siswa
 
-class EntryViewModel(private val repositoriSiswa: RepositoriSiswa): ViewModel(){
+class EntryViewModel(private val repositoriSiswa: RepositoriSiswa) : ViewModel() {
+
+    // Berisi status Siswa saat ini
     var uiStateSiswa by mutableStateOf(UIStateSiswa())
         private set
-    private fun validasiInput(uiState:DetailSiswa = uiStateSiswa.detailSiswa): Boolean {
+
+    // Fungsi untuk memvalidasi input
+    private fun validasiInput(uiState: DetailSiswa = uiStateSiswa.detailSiswa): Boolean {
         return with(uiState) {
             nama.isNotBlank() && alamat.isNotBlank() && telpon.isNotBlank()
         }
     }
 
+    fun updateUIState(detailSiswa: DetailSiswa) {
+        uiStateSiswa =
+            UIStateSiswa(
+                detailSiswa = detailSiswa,
+                isEntryValid = validasiInput(detailSiswa)
+            )
+    }
+
+    // Fungsi untuk menyimpan data yang di-entry
+    suspend fun saveSiswa() {
+        if (validasiInput()) {
+            repositoriSiswa.insertSiswa(uiStateSiswa.detailSiswa.toSiswa())
+        }
+    }
 }
+
+data class UIStateSiswa(
+    val detailSiswa: DetailSiswa = DetailSiswa(),
+    val isEntryValid: Boolean = false
+)
+
 data class DetailSiswa(
     val id: Int = 0,
     val nama: String = "",
     val alamat: String = "",
-    val telpon: String = "",
+    val telpon: String = ""
 )
 
 fun DetailSiswa.toSiswa(): Siswa = Siswa(
@@ -31,10 +56,11 @@ fun DetailSiswa.toSiswa(): Siswa = Siswa(
     telpon = telpon
 )
 
-fun Siswa.toUiStateSiswa(isEntryValid:Boolean = false): UIStateSiswa = UIStateSiswa(
-    detailSiswa = this.toDetailSiswa(),
-    isEntryValid = isEntryValid
-)
+fun Siswa.toUIStateSiswa(isEntryValid: Boolean = false): UIStateSiswa =
+    UIStateSiswa(
+        detailSiswa = this.toDetailSiswa(),
+        isEntryValid = isEntryValid
+    )
 
 fun Siswa.toDetailSiswa(): DetailSiswa = DetailSiswa(
     id = id,
