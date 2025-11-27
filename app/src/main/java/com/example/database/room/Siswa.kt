a00be4fbde0a625ca6283cf6bb4ccc328142b03f
Pradipa Y <pdpyyo@gmail.com>
2025-11-27 15:54:36 +0700

    mengisi Entity pada Siswa.kt

--- app/src/main/java/com/example/database/room/Siswa.kt
+++ app/src/main/java/com/example/database/room/Siswa.kt
@@ -1,9 +1,14 @@
 package com.example.database.room
 
 import androidx.room.Entity
+import androidx.room.PrimaryKey
 
 
 @Entity(tableName = "tblSiswa")
-data class Siswa {
-
-}
+data class Siswa (
+    @PrimaryKey(autoGenerate = true)
+    val id : Int = 0,
+    val nama : String,
+    val alamat : String,
+    val telpon : String
+)
