145b83feb14e4a9e1f6c0795c5fbffcb6bd49fa2
Pradipa Y <pdpyyo@gmail.com>
2025-11-27 16:34:54 +0700

    membuat class HomeViewModel pada HomeViewModel.kt

---
+++ app/src/main/java/com/example/database/viewmodel/HomeViewModel.kt
@@ -0,0 +1,8 @@
+package com.example.database.viewmodel
+
+import androidx.lifecycle.ViewModel
+import com.example.database.repositori.RepositoriSiswa
+
+class HomeViewModel(private val repositoriSiswa: RepositoriSiswa): ViewModel() {
+
+}
