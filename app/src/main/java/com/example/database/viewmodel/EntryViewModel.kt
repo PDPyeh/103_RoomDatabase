acd6c96a6a70b6bf9479d6fdc0e42b455b0f3a9a
Pradipa Y <pdpyyo@gmail.com>
2025-11-27 18:20:46 +0700

    membuat class EntryViewModel pada EntryViewModel.kt

--- app/src/main/java/com/example/database/viewmodel/EntryViewModel.kt
+++ app/src/main/java/com/example/database/viewmodel/EntryViewModel.kt
@@ -1,7 +1,22 @@
 package com.example.database.viewmodel
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import com.example.database.repositori.RepositoriSiswa
 import com.example.database.room.Siswa
 
+class EntryViewModel(private val repositoriSiswa: RepositoriSiswa): ViewModel(){
+    var uiStateSiswa by mutableStateOf(UIStateSiswa())
+        private set
+    private fun validasiInput(uiState:DetailSiswa = uiStateSiswa.detailSiswa): Boolean {
+        return with(uiState) {
+            nama.isNotBlank() && alamat.isNotBlank() && telpon.isNotBlank()
+        }
+    }
+
+}
 data class DetailSiswa(
     val id: Int = 0,
     val nama: String = "",
@@ -21,7 +36,9 @@ fun Siswa.toUiStateSiswa(isEntryValid:Boolean = false): UIStateSiswa = UIStateSi
     isEntryValid = isEntryValid
 )
 
-
 fun Siswa.toDetailSiswa(): DetailSiswa = DetailSiswa(
     id = id,
+    nama = nama,
+    alamat = alamat,
+    telpon = telpon
 )
