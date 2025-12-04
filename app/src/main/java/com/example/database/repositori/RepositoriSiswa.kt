ac91ff8189d61c7a40b1a112473cd6a473cd2134
Pradipa Y <pdpyyo@gmail.com>
2025-12-04 15:21:09 +0700

    tambah override function di offlineRepositoriSiswa

--- app/src/main/java/com/example/database/repositori/RepositoriSiswa.kt
+++ app/src/main/java/com/example/database/repositori/RepositoriSiswa.kt
@@ -9,7 +9,6 @@ interface RepositoriSiswa{
     suspend fun insertSiswa(siswa: Siswa)
     fun getSiswaStream(id: Int): Flow<Siswa?>
     suspend fun deleteSiswa(siswa: Siswa)
-    suspend fun updateSiswa(siswa: Siswa)
 }
 
 class OfflineRepositoriSiswa(
@@ -17,4 +16,6 @@ class OfflineRepositoriSiswa(
 ): RepositoriSiswa {
     override fun getAllSiswaStream(): Flow<List<Siswa>> = siswaDao.getAllSiswa()
     override suspend fun insertSiswa(siswa: Siswa) = siswaDao.insert(siswa)
+    override fun getSiswaStream(id: Int): Flow<Siswa?> = siswaDao.getSiswa(id)
+    override suspend fun deleteSiswa(siswa: Siswa) = siswaDao.delete(siswa)
 }
